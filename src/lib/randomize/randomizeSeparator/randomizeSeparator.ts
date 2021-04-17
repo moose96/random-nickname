2963c134469f2c8559a85f2b49327bbaa389bf8e
Piotr Łosiak <piotr.losiak96@gmail.com>
2021-04-17 13:26:04 +0200

    fix: throwing error when selected is empty string

--- src/lib/randomize/randomizeSeparator/randomizeSeparator.ts
+++ src/lib/randomize/randomizeSeparator/randomizeSeparator.ts
@@ -1,6 +1,6 @@
 export default function randomizeSeparator(separators: string[]): string {
   const separator = separators[Math.floor(Math.random() * separators.length)];
-  if (separator) {
+  if (typeof separator === 'string') {
     return separator;
   } else {
     throw Error('[randomizeSeparator]: separator is undefined');
